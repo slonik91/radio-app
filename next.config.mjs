ab729430c557775c78b51bc4dada3f30e724236d
slonik91 <xion4ik91@gmail.com>
2025-09-30 00:07:19 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    // App Router is default in Next 13/14; kept here for clarity
-  },
+  reactStrictMode: true
 };
 
 export default nextConfig;
