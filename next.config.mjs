243bf855b1f327d2e531648e6ac99c7fee231e88
slonik91 <xion4ik91@gmail.com>
2025-09-30 00:16:50 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+
+  // делает сборку самодостаточной (удобно для контейнеров/хостингов)
+  output: "standalone",
+
+  // если вдруг будешь добавлять оптимизацию изображений через <Image/>,
+  // сюда можно перечислить свои домены-картинок:
+  // images: { domains: ["example.com"] },
 };
 
 export default nextConfig;
